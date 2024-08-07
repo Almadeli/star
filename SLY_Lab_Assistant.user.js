a04a732042dd7ed55890f7a26780fed84c8ddb9c
Almadeli <almadeli1991@gmail.com>
2024-08-07 04:02:14 +0200

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/Almadeli/star/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/Almadeli/star/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/Almadeli/star/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -20,7 +20,7 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc'];
+    let rpcEndpoints = ['https://solana-mainnet.g.alchemy.com/v2/K8jLOkE-uTxsELdIwnz4qjsa-GNH1vOr','https://rpc.hellomoon.io/7c543c15-6018-4d59-82dc-be833b80c8a6'];
     const priorityFee = 1; // Priority Fee added to each transaction in Lamports. Set to 0 (zero) to disable priority fees. 1 Lamport = 0.000000001 SOL
 
     const connectionProxy = {
@@ -731,7 +731,7 @@
 
             let latestBH = await solanaConnection.getLatestBlockhash('confirmed');
             tx.recentBlockhash = latestBH.blockhash;
-            tx.lastValidBlockHeight = latestBH.lastValidBlockHeight-150;
+            tx.lastValidBlockHeight = latestBH.lastValidBlockHeight-200;
             tx.feePayer = userPublicKey;
             tx.signer = userPublicKey;
             let txSigned = null;
@@ -755,8 +755,8 @@
             let txHash = response.txHash;
             let confirmation = response.confirmation;
             let txResult = await solanaConnection.getTransaction(txHash, {commitment: 'confirmed', preflightCommitment: 'confirmed', maxSupportedTransactionVersion: 1});
-            //let priorityHistory = await solanaConnection.getRecentPrioritizationFees();
-            //console.log('priorityHistory: ', priorityHistory);
+            /*let priorityHistory = await solanaConnection.getRecentPrioritizationFees();
+            console.log('priorityHistory: ', priorityHistory);*/
             if (confirmation.name == 'TransactionExpiredBlockheightExceededError' && !txResult) {
                 console.log('-----RETRY-----');
                 txResult = await txSignAndSend(ix);
